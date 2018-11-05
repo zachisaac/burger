5871cd7d82a99800fa95aa44ae7d1f088692a3b7
zpaki5142 <zachisaac@users.noreply.github.com>
2018-11-05 15:46:13 -0800

    third commit

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE burgers_db;
 USE  burgers_db;
 
 CREATE TABLE burgers (
-id int NOT NULL IDENTITY,
+id int NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR (45) NOT NULL,
-devoured BOOLEAN,
+devoured BOOLEAN DEFAULT false,
 PRIMARY KEY (id) 
 );
